aef96af3842b60d6e2193ced40559d796b35bf0b
Preston Doris <preston@prestondoris.com>
2018-01-28 17:51:56 -0800

    Add initial Foursquare API connection

--- findARestaurant.py
+++ findARestaurant.py
@@ -1,6 +1,7 @@
 from geocode import getGeocodeLocation
 import json
 import httplib2
+import requests
 
 import sys
 import codecs
@@ -13,11 +14,22 @@ foursquare_client_secret = "M0UPWFB0MPWC5UR5H51K3WDUXZJACCIVYX4ENUNOAOLUYYSL"
 
 def findARestaurant(mealType,location):
 	#1. Use getGeocodeLocation to get the latitude and longitude coordinates of the location string.
-    getLatLon = getGeocodeLocation(location)
-    print getLatLon
+    latLon = '%s, %s' % getGeocodeLocation(location)
 
 	#2.  Use foursquare API to find a nearby restaurant with the latitude, longitude, and mealType strings.
 	#HINT: format for url will be something like https://api.foursquare.com/v2/venues/search?client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=20130815&ll=40.7,-74&query=sushi
+    url_FS = 'https://api.foursquare.com/v2/venues/explore'
+    params = dict(
+        client_id=foursquare_client_id,
+        client_secret=foursquare_client_secret,
+        v='20170801',
+        ll=latLon,
+        query=mealType,
+        limit=1
+    )
+    resp = requests.get(url=url_FS, params=params)
+    data = json.loads(resp.text)
+    print data
 
 	#3. Grab the first restaurant
 	#4. Get a  300x300 picture of the restaurant using the venue_id (you can change this by altering the 300x300 value in the URL or replacing it with 'orginal' to get the original picture
@@ -27,11 +39,11 @@ def findARestaurant(mealType,location):
 
 if __name__ == '__main__':
 	findARestaurant("Pizza", "Tokyo, Japan")
-	findARestaurant("Tacos", "Jakarta, Indonesia")
-	findARestaurant("Tapas", "Maputo, Mozambique")
-	findARestaurant("Falafel", "Cairo, Egypt")
-	findARestaurant("Spaghetti", "New Delhi, India")
-	findARestaurant("Cappuccino", "Geneva, Switzerland")
-	findARestaurant("Sushi", "Los Angeles, California")
-	findARestaurant("Steak", "La Paz, Bolivia")
-	findARestaurant("Gyros", "Sydney Australia")
+	#findARestaurant("Tacos", "Jakarta, Indonesia")
+	#findARestaurant("Tapas", "Maputo, Mozambique")
+	#findARestaurant("Falafel", "Cairo, Egypt")
+	#findARestaurant("Spaghetti", "New Delhi, India")
+	#findARestaurant("Cappuccino", "Geneva, Switzerland")
+	#findARestaurant("Sushi", "Los Angeles, California")
+	#findARestaurant("Steak", "La Paz, Bolivia")
+	#findARestaurant("Gyros", "Sydney Australia")
