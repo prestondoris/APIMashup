cfccaa4e7b87246d86b3edb402dbd6cc78b6d91f
Preston Doris <preston@prestondoris.com>
2018-01-28 18:44:08 -0800

    Parse Foursquare response
    Parsed the Foursquare response for the 1st restaurant name and address

--- findARestaurant.py
+++ findARestaurant.py
@@ -18,18 +18,23 @@ def findARestaurant(mealType,location):
 
 	#2.  Use foursquare API to find a nearby restaurant with the latitude, longitude, and mealType strings.
 	#HINT: format for url will be something like https://api.foursquare.com/v2/venues/search?client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=20130815&ll=40.7,-74&query=sushi
-    url_FS = 'https://api.foursquare.com/v2/venues/explore'
+    url_FS = 'https://api.foursquare.com/v2/venues/search'
     params = dict(
         client_id=foursquare_client_id,
         client_secret=foursquare_client_secret,
         v='20170801',
         ll=latLon,
         query=mealType,
-        limit=1
+        intent='browse',
+        radius=16000,
+        limit=5
     )
     resp = requests.get(url=url_FS, params=params)
     data = json.loads(resp.text)
-    print data
+    name = data['response']['venues'][0]['name']
+    print name
+    address = data['response']['venues'][0]['location']['address']
+    print address
 
 	#3. Grab the first restaurant
 	#4. Get a  300x300 picture of the restaurant using the venue_id (you can change this by altering the 300x300 value in the URL or replacing it with 'orginal' to get the original picture
@@ -38,12 +43,12 @@ def findARestaurant(mealType,location):
 	#7. Return a dictionary containing the restaurant name, address, and image url
 
 if __name__ == '__main__':
-	findARestaurant("Pizza", "Tokyo, Japan")
+	#findARestaurant("Pizza", "Tokyo, Japan")
 	#findARestaurant("Tacos", "Jakarta, Indonesia")
 	#findARestaurant("Tapas", "Maputo, Mozambique")
 	#findARestaurant("Falafel", "Cairo, Egypt")
 	#findARestaurant("Spaghetti", "New Delhi, India")
 	#findARestaurant("Cappuccino", "Geneva, Switzerland")
-	#findARestaurant("Sushi", "Los Angeles, California")
+	findARestaurant("Sushi", "Los Angeles, California")
 	#findARestaurant("Steak", "La Paz, Bolivia")
 	#findARestaurant("Gyros", "Sydney Australia")
